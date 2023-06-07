20c07c7cc8138214345379a96c1e50a8c80ddcf1
ajuteixeira <teixeirajuuh@gmail.com>
2023-06-07 09:43:46 -0300

    form validation

--- script.js
+++ script.js
@@ -23,36 +23,66 @@ const cidade = document.getElementById('cidade');
 const invalidoCidade = document.getElementById('invalido-cidade');
 const cep = document.getElementById('cep');
 const invalidoCep = document.getElementById('invalido-cep');
+const btn = document.getElementById('form-botao')
+let formPlacaValido = false
+let formCidadeValido = false
 
-formulario.addEventListener('submit', (e) => {
-    e.preventDefault();
-    if (placa.value.length === 7) {
+function validarPlacaInput() {
+    const formatoPlaca = /^[0-9]{7}$/;
+    if (placa.value.match(formatoPlaca)) {
         console.log('true', placa.value.length);
         placa.style.border = '1px solid green';
         invalidoPlaca.style.display = 'none'
+        formPlacaValido = true;
+        validaBotaoSubmit()
     } else {
         console.log('false', placa.value.length);
         placa.style.border = '1px solid red';
         invalidoPlaca.style.display = 'block';
     }
+}
 
+function validarCidadeInput() {
     if (cidade.value) {
-        console.log('inválido!')
-        cidade.style.border = '1px solid red';
-        invalidoCidade.style.display = 'block'
-    } else {
         console.log('valido!')
         cidade.style.border = '1px solid green';
-        invalidoCidade.style.display = 'none'
+        invalidoCidade.style.display = 'none';
+        formCidadeValido = true;
+        validaBotaoSubmit()
+    } else {
+        console.log('inválido!')
+        cidade.style.border = '1px solid red';
+        invalidoCidade.style.display = 'block';
     }
+}
 
-    if (cep.value.length === 8) {
+function validarCepInput() {
+    const formatoCep = /^[0-9]{8}$/;
+    if (cep.value.match(formatoCep)) {
         console.log('true', cep.value.length);
         cep.style.border = '1px solid green';
-        invalidoCep.style.display = 'none'
+        invalidoCep.style.display = 'none';
     } else {
         console.log('false', cep.value.length);
         cep.style.border = '1px solid red';
         invalidoCep.style.display = 'block';
     }
-})
+}
+
+function validaBotaoSubmit() {
+    if (formPlacaValido && formCidadeValido) {
+        btn.removeAttribute('disabled')
+    }
+}
+
+placa.addEventListener('change', validarPlacaInput)
+cidade.addEventListener('change', validarCidadeInput)
+cep.addEventListener('change', validarCepInput)
+
+// formulario.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     validarPlacaInput();
+//     validarCidadeInput();
+//     validarCepInput();
+//     validaBotaoSubmit();
+// })
